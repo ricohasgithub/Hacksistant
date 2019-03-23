b00ba3a0ccbdf6608917f9c85d705394144784e2
ricozhuthegreat <33135503+ricozhuthegreat@users.noreply.github.com>
2019-03-23 18:29:02 -0400

    Added Input Tensors
     - Aded input tensors for the toolkit memory network

--- Memory_Network.py
+++ Memory_Network.py
@@ -10,6 +10,7 @@ from keras.layers import Input, LSTM, Dense
 from keras.preprocessing.text import text_to_word_sequence
 from keras.preprocessing.text import Tokenizer
 
+
 # Read technology toolkits data from csv file into panda table
 tech_toolkits_data = pd.read_csv("datasets/tech_toolkits.csv")
 print(tech_toolkits_data.head())
@@ -18,6 +19,39 @@ print(tech_toolkits_data.describe())
 
 tech_toolkits_data_columns = tech_toolkits_data.columns
 
+# Toolkit name
+tech_toolkit_names = tech_toolkits_data[tech_toolkits_data_columns[0]]
+print(tech_toolkit_names)
+
+# Toolkit int value
+tech_toolkit_vals = tech_toolkits_data[tech_toolkits_data_columns[1]]
+print(tech_toolkit_vals)
+
+# Associated language of toolkit (in int value of language)
+tech_toolkit_asclan = tech_toolkits_data[tech_toolkits_data_columns[2]]
+print(tech_toolkit_asclan)
+
+# Type of program (e.g., app, website, mobile .etc)
+tech_toolkit_type = tech_toolkits_data[tech_toolkits_data_columns[3]]
+print(tech_toolkit_type)
+
+
+# Read training data with results
+tech_toolkits_train = pd.read_csv("datasets/tech_toolkit_train.csv")
+
+tech_toolkits_train_columns = tech_toolkits_train.columns
+
+# Type of desired program
+tech_toolkits_train_type = tech_toolkits_train[tech_toolkits_train_columns[0]]
+print(tech_toolkits_train_type)
+
+# Language predicter data
+tech_toolkits_train_lan = tech_toolkits_train[tech_toolkits_train_columns[1:9]]
+print(tech_toolkits_train_lan)
+
+# Toolkit use target data
+tech_toolkits_train_tar = tech_toolkits_train[tech_toolkits_train_columns[9:19]]
+print(tech_toolkits_train_tar)
 
 
 # Model to get useful toolkites to use based on technologies familiar
@@ -28,9 +62,9 @@ tech_toolkits_data_columns = tech_toolkits_data.columns
 def tech_regression_model():
     # Create model
     model = Sequential()
-    model.add(Dense(7, activation='relu', input_shape=()))
-    model.add(Dense(50, activation='relu'))
-    model.add(Dense(21))
+    model.add(Dense(9, activation='relu', input_shape=()))
+    model.add(Dense(27, activation='relu'))
+    model.add(Dense(8))
     
     # Compile model
     model.compile(optimizer='adam', loss='mean_squared_error')
@@ -59,7 +93,7 @@ print(tok.word_docs)
 tech_docs = [
     "python",
     "java",
-    "javascipt",
+    "javascript",
     "website",
     "android",
     "ios",
