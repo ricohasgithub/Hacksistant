5e424bd94d597f3fea7a401f8b016d70631698c5
Jonah Walker <jonahlwalker@gmail.com>
2019-03-24 10:31:22 -0400

    trying to write to db

--- gActions/functions/index.js
+++ gActions/functions/index.js
@@ -2,6 +2,26 @@
 // for Dialogflow fulfillment library docs, samples, and to report issues
 'use strict';
 
+// Initialize Firebase
+var firebase = require("firebase");
+var admin = require('firebase-admin');
+var fs = require('fs');
+var apiKey = "";
+fs.readFile('secret.txt', function(err, data) {
+	if (err) throw err;
+	apiKey = data.toString();
+});
+var serviceAccount = require('./secret.json');
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: 'https://last-second-hack-128ce.firebaseio.com/'
+});
+var config = {
+  apiKey: apiKey,
+  databaseURL: "https://last-second-hack-128ce.firebaseio.com/"
+};
+//admin.initializeApp(config);
+
 // Import the Dialogflow module from the Actions on Google client library.
 const {dialogflow} = require('actions-on-google');
 
@@ -11,10 +31,21 @@ const functions = require('firebase-functions');
 // Instantiate the Dialogflow client.
 const app = dialogflow({debug: true});
 
+// Get a reference to the database service
+//var database = firebase.database();
+
+// Get a database reference
+var db = admin.database();
+var ref = db.ref("server/");
+
+var response = ref.child("done");
+
 // Make some global variables
 var lang = "";
 var tpc = "";
 var plt = "";
+
+var done = false;
 
 // Handle the Dialogflow intents.
 // The intents collect parameters.
@@ -31,6 +62,8 @@ app.intent('possible topic', (conv, {topic}) => {
 app.intent('platform to use', (conv, {platform}) => {
 	plt = platform;
     conv.close('Looking for potential ' + plt + ' projects written in ' + lang + ' and with the topic of ' + tpc + '...');
+	//response.update(true);
+	//firebase.database().ref('done').set(true);
 });
 // Set the DialogflowApp object to handle the HTTPS POST request.
 exports.dialogflowFirebaseFulfillment = functions.https.onRequest(app);
