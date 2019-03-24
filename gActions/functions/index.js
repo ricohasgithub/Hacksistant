9d311c2ba417c216f2d16fdc244d1305ecbb5e44
Jonah Walker <jonahlwalker@gmail.com>
2019-03-24 08:28:09 -0400

    finalizing input

--- gActions/functions/index.js
+++ gActions/functions/index.js
@@ -11,16 +11,26 @@ const functions = require('firebase-functions');
 // Instantiate the Dialogflow client.
 const app = dialogflow({debug: true});
 
+// Make some global variables
+var lang = "";
+var tpc = "";
+var plt = "";
+
 // Handle the Dialogflow intents.
 // The intents collect parameters.
 app.intent('language to use', (conv, {pLanguage}) => {
+	lang = pLanguage;
     const luckyNumber = pLanguage.length;
-    // Respond with the user's lucky number and end the conversation.
     //conv.add('Did you know the language ' + pLanguage + ' is ' + luckyNumber + ' characters long? Also, Hello World!');
-	conv.ask('Did you know the language ' + pLanguage + ' is ' + luckyNumber + ' characters long? Also, Hello World!\n\nName a topic you might be interested in for your hackathon.');
+	conv.ask(lang + ' is one of my favourite languages!\n\nNow, name a topic you might be interested in for your hackathon.');
 });
 app.intent('possible topic', (conv, {topic}) => {
-    conv.close('Alright, i like the topic of ' + topic + ' too!');
+	tpc = topic;
+    conv.ask('Alright, I like the topic of ' + topic + ' too!\n\nFinally, what platform would you like to use?');
+});
+app.intent('platform to use', (conv, {platform}) => {
+	plt = platform;
+    conv.close('Looking for potential ' + plt + ' projects written in ' + lang + ' and with the topic of ' + tpc + '...');
 });
 // Set the DialogflowApp object to handle the HTTPS POST request.
 exports.dialogflowFirebaseFulfillment = functions.https.onRequest(app);
